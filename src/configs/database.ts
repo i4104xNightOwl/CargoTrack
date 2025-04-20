32471b9d24a11bcfdf1c86c1f6731af4c82cc124
I4104 Night Owl <I4104.vn@gmail.com>
2025-04-21 01:17:06 +0700

    REFACTOR(database): Chuyển sang sử dụng sequelize

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,47 +1,18 @@
-import mysql, { Connection } from 'mysql2';
-import dotenv from 'dotenv';
+require('dotenv').config()
 
-dotenv.config();
+const host = process.env.DB_HOST
+const username = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const database = process.env.DB_NAME
 
-export default class Database {
-    private static instance: Database;
-    private connection: Connection;
-
-    private constructor() {
-        this.connection = mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-        });
-
-        this.connection.connect((err) => {
-            if (err) {
-                console.error('❌ Error connecting to MySQL:', err.message);
-                return;
-            }
-            console.log('✅ Connected to MySQL database');
-        });
-    }
-
-    public static getInstance(): Database {
-        if (!Database.instance) {
-            Database.instance = new Database();
-        }
-        return Database.instance;
-    }
-
-    public getConnection(): Connection {
-        return this.connection;
-    }
-
-    public disconnect(): void {
-        this.connection.end((err) => {
-            if (err) {
-                console.error('❌ Error disconnecting MySQL:', err.message);
-                return;
-            }
-            console.log('🔌 Disconnected from MySQL');
-        });
-    }
+module.exports = {
+	development: {
+		username,
+		password,
+		database,
+		host,
+		port: 3306,
+		dialect: 'mysql',
+	},
 }
+
---
+++ src/configs/models/users.model.ts
@@ -0,0 +1,94 @@
+import { DataTypes, Model, ModelScopeOptions, ModelValidateOptions, Optional } from 'sequelize'
+import { sequelize } from '.'
+
+// Định nghĩa các attributes cho model
+export interface UsersAttributes {
+    id: number
+    username: string
+    password: string
+    email: string
+    phone: string
+    role: string
+    status: number
+    createdAt: Date
+    updatedAt: Date
+}
+
+// Loại bỏ ID khi thêm dữ liệu
+export interface UsersCreationAttributes extends Optional<UsersAttributes, 'id'> { }
+
+export class Users extends Model<UsersAttributes, UsersCreationAttributes> implements UsersAttributes {
+    id: number
+    username: string
+    password: string
+    email: string
+    phone: string
+    role: string
+    status: number
+    createdAt: Date
+    updatedAt: Date
+    
+    static readonly scopes: ModelScopeOptions = {
+        /*
+          nơi khai báo scope
+        */
+    }
+
+    static readonly validations: ModelValidateOptions = {
+        /*
+          nơi khai báo validation
+        */
+    }
+}
+
+Users.init({
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+    },
+    username: {
+        allowNull: false,
+        type: DataTypes.STRING,
+    },
+    password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+    },
+    email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+    },
+    phone: {
+        allowNull: false,
+        type: DataTypes.STRING,
+    },
+    role: {
+        allowNull: false,
+        type: DataTypes.STRING,
+    },
+    status: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+    },
+    createdAt: {
+        allowNull: true,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+        allowNull: true,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+}, {
+    sequelize,
+    tableName: 'users',
+    underscored: true,
+    updatedAt: true,
+    createdAt: true,
+    scopes: Users.scopes,
+    validate: Users.validations,
+})
+
