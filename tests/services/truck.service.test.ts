a9ab07d31d9577b1b34bf1e8b9c8194fb7f17434
I4104 Night Owl <I4104.vn@gmail.com>
2025-04-20 22:14:53 +0700

    FEAT(Backend): Service base and Test Base for Service

---
+++ tests/services/truck.service.test.ts
@@ -0,0 +1,22 @@
+
+describe("Kiểm tra TruckService", () => {
+    it("Kiểm tra get", () => {
+
+    });
+
+    it("Kiểm tra getById", () => {
+
+    });
+
+    it("Kiểm tra create", () => {
+
+    });
+
+    it("Kiểm tra update", () => {
+
+    });
+
+    it("Kiểm tra delete", () => {
+
+    });
+})
