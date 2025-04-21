b0d3a138114124c772e1a6ae27e1f8404222a80f
I4104 Night Owl <I4104.vn@gmail.com>
2025-04-21 08:01:28 +0700

    REFACTOR(Service): Sửa lại code phần service để sử dụng model và viết lại bài test

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,22 +1,25 @@
 import { IUsers } from 'interfaces/models/users.model';
 import { IUsersService } from '../../interfaces/services/users.service';
 import { UsersDB } from '@configs/models/users.model';
+import { plainToInstance } from 'class-transformer';
+import Users from '@src/models/users.model';
 
 export default class UsersService implements IUsersService {
     async get(): Promise<IUsers[]> {
         const users = (await UsersDB.findAll()).map((user: UsersDB) => {
-            return user.dataValues
+            return plainToInstance(Users, user.dataValues); 
         });
         return users;
     }
     async getById(id: number): Promise<IUsers> {
         const users = await UsersDB.findByPk(id);
         if (!users) throw new Error('User not found');
-        return users.dataValues;
+        return plainToInstance(Users, users.dataValues);
     }
 
     async create(item: IUsers): Promise<IUsers> {
-        return await UsersDB.create(item);
+        const users = await UsersDB.create(item);
+        return plainToInstance(Users, users.dataValues);
     }
 
     async update(item: IUsers): Promise<IUsers> {
--- tests/services/users.service.test.ts
+++ tests/services/users.service.test.ts
@@ -2,6 +2,7 @@ import { sequelize } from '../../src/configs/models';
 import { IUsers, UserStatus } from '../../interfaces/models/users.model';
 import UsersService from '../../src/services/users.service';
 import nomalizeDate from '../../utils/dateUtils';
+import { UsersBuilder } from '../../src/models/users.model';
 
 describe("Kiểm tra UserService", () => {
     beforeAll(async () => {
@@ -17,113 +18,108 @@ describe("Kiểm tra UserService", () => {
     })
 
     it("Kiểm tra get", async () => {
-        // Demo users
-        const user: IUsers = {
-            id: 1,
-            username: "username",
-            password: "password",
-            email: "email",
-            phone: "phone",
-            role: "role",
-            status: UserStatus.ACTIVE,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
+        const user: IUsers = UsersBuilder.new()
+            .setUsername("username")
+            .setPassword("password")
+            .setEmail("email")
+            .setPhone("phone")
+            .setRole("role")
+            .setStatus(UserStatus.ACTIVE)
+            .build();
 
         const usersService = new UsersService();
-        await usersService.create(user);
+        const createdUser = await usersService.create(user);
 
         const getUser = await usersService.get();
-        
-        // TODO: Xóa phần này sau khi viết phần model
-        user.status = 1;
 
         expect(getUser.length).toBeGreaterThan(0);
         expect(nomalizeDate(getUser)).toEqual(
-            expect.arrayContaining([nomalizeDate(user)])
+            expect.arrayContaining([nomalizeDate(createdUser)])
         );
     });
 
     it("Kiểm tra getById", async () => {
-        const user: IUsers = {
-            id: 1,
-            username: "username",
-            password: "password",
-            email: "email",
-            phone: "phone",
-            role: "role",
-            status: UserStatus.ACTIVE,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
+        const user: IUsers = UsersBuilder.new()
+            .setUsername("username")
+            .setPassword("password")
+            .setEmail("email")
+            .setPhone("phone")
+            .setRole("role")
+            .setStatus(UserStatus.ACTIVE)
+            .build();
 
         const usersService = new UsersService();
-        await usersService.create(user);        
+        const createdUser = await usersService.create(user);        
 
-        const getUser = await usersService.getById(user.id);
-        expect(nomalizeDate(getUser)).toEqual(nomalizeDate(user));
+        const getUser = await usersService.getById(createdUser.id);
+        
+        expect(getUser.username).toEqual(user.username);
+        expect(getUser.email).toEqual(user.email);
+        expect(getUser.phone).toEqual(user.phone);
+        expect(getUser.password).toEqual(user.password);
+        expect(getUser.role).toEqual(user.role);
+        expect(getUser.status).toEqual(user.status);
     });
 
     it("Kiểm tra create", async () => {
-        const user: IUsers = {
-            id: 1,
-            username: "username",
-            password: "password",
-            email: "email",
-            phone: "phone",
-            role: "role",
-            status: UserStatus.ACTIVE,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
+        const user: IUsers = UsersBuilder.new()
+            .setUsername("username")
+            .setPassword("password")
+            .setEmail("email")
+            .setPhone("phone")
+            .setRole("role")
+            .setStatus(UserStatus.ACTIVE)
+            .build();
 
         const usersService = new UsersService();
-        await usersService.create(user);
+        const createdUser = await usersService.create(user);
 
-        const getUser = await usersService.getById(user.id);
-        expect(nomalizeDate(getUser)).toEqual(nomalizeDate(user));
+        const getUser = await usersService.getById(createdUser.id);
+
+        expect(getUser.username).toEqual(user.username);
+        expect(getUser.email).toEqual(user.email);
+        expect(getUser.phone).toEqual(user.phone);
+        expect(getUser.password).toEqual(user.password);
+        expect(getUser.role).toEqual(user.role);
+        expect(getUser.status).toEqual(user.status);
     });
 
     it("Kiểm tra update", async () => {
-        const user: IUsers = {
-            id: 1,
-            username: "username",
-            password: "password",
-            email: "email",
-            phone: "phone",
-            role: "role",
-            status: UserStatus.ACTIVE,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
+        const user: IUsers = UsersBuilder.new()
+            .setUsername("username")
+            .setPassword("password")
+            .setEmail("email")
+            .setPhone("phone")
+            .setRole("role")
+            .setStatus(UserStatus.ACTIVE)
+            .build();
 
         const usersService = new UsersService();
         await usersService.create(user);
 
-        const getUser = await usersService.getById(user.id);
+        const createdUser = await usersService.create(user);
+        const getUser = await usersService.getById(createdUser.id);
+
         getUser.username = "new username";
         await usersService.update(getUser);
 
-        const getUserUpdated = await usersService.getById(user.id);
+        const getUserUpdated = await usersService.getById(createdUser.id);
         expect(getUserUpdated).toEqual(getUser);
     });
 
     it("Kiểm tra delete", async () => {
-        const user: IUsers = {
-            id: 1,
-            username: "username",
-            password: "password",
-            email: "email",
-            phone: "phone",
-            role: "role",
-            status: UserStatus.ACTIVE,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
+        const user: IUsers = UsersBuilder.new()
+            .setUsername("username")
+            .setPassword("password")
+            .setEmail("email")
+            .setPhone("phone")
+            .setRole("role")
+            .setStatus(UserStatus.ACTIVE)
+            .build();
 
         const usersService = new UsersService();
-        await usersService.create(user);
-        await usersService.delete(user);
+        const createdUser = await usersService.create(user);
+        await usersService.delete(createdUser);
 
         const getAll = await usersService.get();
         expect(getAll.length).toBe(0);
