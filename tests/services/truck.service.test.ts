fe814f33aeb54cd1470744dcea1e8ae4e38f0fed
I4104 Night Owl <I4104.vn@gmail.com>
2025-04-22 17:38:27 +0700

    FEAT(Test): Viết bài test cho phần service

--- tests/services/truck.service.test.ts
+++ tests/services/truck.service.test.ts
@@ -1,3 +1,8 @@
+import { TruckStatus } from "../../interfaces/models/truck.model";
+import { sequelize } from "../../src/configs/models";
+import { TruckBuilder } from "../../src/models/truck.model";
+import TruckService from "../../src/services/truck.service";
+import nomalizeDate from "../../utils/dateUtils";
 
 describe("Kiểm tra TruckService", () => {
     beforeAll(async () => {
@@ -12,23 +17,74 @@ describe("Kiểm tra TruckService", () => {
         await sequelize.close();
     })
     
-    it("Kiểm tra get", () => {
-        
+    it("Kiểm tra get", async () => {
+        const truck = TruckBuilder.new()
+            .setLicensePlate("licensePlate")
+            .setStatus(TruckStatus.NotUsed).build();    
+
+        const truckService = new TruckService();
+        const createdTruck = await truckService.create(truck);
+        const getTruck = await truckService.get();
+
+        expect(getTruck.length).toBeGreaterThan(0);
+        expect(nomalizeDate(getTruck)).toEqual(nomalizeDate([createdTruck]));
     });
 
-    it("Kiểm tra getById", () => {
+    it("Kiểm tra getById", async () => {
+        const truck = TruckBuilder.new()
+            .setLicensePlate("licensePlate")
+            .setStatus(TruckStatus.NotUsed).build();
 
+        const truckService = new TruckService();
+        const createdTruck = await truckService.create(truck);
+
+        const getTruck = truckService.getById(createdTruck.id);
+
+        expect(getTruck).toEqual(createdTruck);
     });
 
-    it("Kiểm tra create", () => {
+    it("Kiểm tra create", async () => {
+        const truck = TruckBuilder.new()
+            .setLicensePlate("licensePlate")
+            .setStatus(TruckStatus.NotUsed).build();
 
+        const truckService = new TruckService();
+        const createdTruck = await truckService.create(truck);
+
+        expect(createdTruck.licensePlate).toEqual(truck.licensePlate);
+        expect(createdTruck.status).toEqual(truck.status);
     });
 
-    it("Kiểm tra update", () => {
+    it("Kiểm tra update", async () => {
+        const truck = TruckBuilder.new()
+            .setLicensePlate("licensePlate")
+            .setStatus(TruckStatus.NotUsed).build();
 
+        const truckService = new TruckService();
+        const createdTruck = await truckService.create(truck);
+
+        createdTruck.licensePlate = "new licensePlate";
+        createdTruck.status = TruckStatus.IsUsed;
+        await truckService.update(createdTruck);
+
+        const getTruck = await truckService.getById(createdTruck.id);
+
+        expect(getTruck.licensePlate).toEqual("new licensePlate");
+        expect(getTruck.status).toEqual(TruckStatus.IsUsed);
     });
 
-    it("Kiểm tra delete", () => {
+    it("Kiểm tra delete", async () => {
+        const truck = TruckBuilder.new()
+            .setLicensePlate("licensePlate")
+            .setStatus(TruckStatus.NotUsed).build();
 
+        const truckService = new TruckService();
+        const createdTruck = await truckService.create(truck);
+
+        await truckService.delete(createdTruck);
+
+        const getTruck = await truckService.getById(createdTruck.id);
+
+        expect(getTruck).toBeNull();
     });
 })
